a2112c291273874d0b14f8c2283c5ad48036d0ea
Muhammad Bilal Ellahi <bilalillahi25@gmail.com>
2025-04-23 14:29:51 -0700

    Implement gathering feature with CRUD operations and real-time attendance tracking

--- index.js
+++ index.js
@@ -164,6 +164,7 @@ const aiRouter = require('./routes/aiRoutes/ai.routes.js');
 const eventRouter = require('./routes/GPS/event.attendance.route.js')(io);
 
 const locationRouter= require('./routes/GPS/location.sharing.route.js')
+const gatheringRouter= require('./routes/GPS/gathering.route.js')
 
 
 
@@ -198,6 +199,8 @@ app.use("/api/ai", protectRoute, aiRouter);
 
 
 app.use("/api/location", protectRoute, locationRouter);
+// app.use("/api/gatherings", protectRoute, gatheringRouter);
+app.use("/api/gatherings", gatheringRouter);
 app.use("/api/event", protectRoute, eventRouter);
 
 // cafe role doesnot exist
